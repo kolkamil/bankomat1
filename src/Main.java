9c8156c20ec9426a6928d837e1d51b985e517c6e
Kamil <kamilek1490@gmail.com>
2021-04-18 21:43:46 +0200

    switch kończący prace programu

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,15 @@ public class Main {
         Bankomat bankomat = new Bankomat();
         Scanner scanner = new Scanner(System.in);
         boolean kontynuuj = true;
+        bankomat.wyborKonta();
+
         while(kontynuuj) {
-            bankomat.wyborKonta();
+            System.out.println("Czy chcesz zakończyć działanie? \n 1.Tak \n 2.Nie");
+            int wybor = scanner.nextInt();
+            switch(wybor){
+                case 1 -> kontynuuj = false;
+                case 2 ->  bankomat.wyborKonta();
+            }
         }
     }
 
